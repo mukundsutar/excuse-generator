e840c4fc75476ff4c7f0199b05e078ddb15e528c
Mukund Sutar <mukundsutar1512@gmail.com>
2023-08-26 23:09:42 +0530

    Made update ecuse, feel and please functions

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 window.onload = async function () {
-    let newElement = document.createElement("INPUT");
-    newElement.setAttribute("type", "checkbox");
-
-    document.getElementById("please-body").appendChild(newElement);
-
-    newElement.id= "please1";
-
-    document.getElementById("please1").innerText= "Forgive Me.";
-
-    console.log(newElement);
+    await updatePlease();
 };
+
+async function updatePlease(){
+    let arrayPlease = ["Forgive Me.", "Be patient with me.", "Give me another chance.", "Forget it ever happened.", "Do not try to get even.", "Do not call the police.", "Do not tell my parents."]
+
+    for (let id = 0; id < array.length; id++) {
+        const element = array[id];
+        
+    }
+    let newInput = document.createElement("INPUT");
+    newInput.setAttribute("type", "checkbox");
+    let newLabel = document.createElement("label");
+
+    document.getElementById("please-body").appendChild(newInput);
+    document.getElementById("please-body").appendChild(newLabel);
+
+    newLabel.id= "please1";
+
+    let str="Forgive Me."
+    newLabel.innerText= str;
+}
