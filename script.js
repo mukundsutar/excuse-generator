0c0b69391c017e38477e807587e9e255abf11b3b
Mukund Sutar <mukundsutar1512@gmail.com>
2023-08-26 23:10:18 +0530

    Made checkbox update js

--- script.js
+++ script.js
@@ -1,23 +1,89 @@
 window.onload = async function () {
+    await updateExcuses();
+    await updateFeel();
     await updatePlease();
 };
 
-async function updatePlease(){
-    let arrayPlease = ["Forgive Me.", "Be patient with me.", "Give me another chance.", "Forget it ever happened.", "Do not try to get even.", "Do not call the police.", "Do not tell my parents."]
+async function updateExcuses() {
+    let arrayPlease = [
+        "It is a Habit.",
+        "I am a Naturally selfish person.",
+        "I thought it would be funny.",
+        "I didn't know it bothered you.",
+        "You were never supposed to know.",
+        "I was Hungry Lonely Intoxicated.",
+        "It was my evil twin.",
+    ];
 
-    for (let id = 0; id < array.length; id++) {
-        const element = array[id];
-        
+    for (let i = 0; i < arrayPlease.length; i++) {
+        const labelStr = arrayPlease[i];
+
+        let newInput = document.createElement("INPUT");
+        newInput.setAttribute("type", "checkbox");
+        let newLabel = document.createElement("label");
+        let breakLine = document.createElement("br");
+
+        document.getElementById("excuse-body").appendChild(newInput);
+        document.getElementById("excuse-body").appendChild(newLabel);
+        document.getElementById("excuse-body").appendChild(breakLine);
+
+        newLabel.id = "excuse" + i;
+        newLabel.innerText = labelStr;
     }
-    let newInput = document.createElement("INPUT");
-    newInput.setAttribute("type", "checkbox");
-    let newLabel = document.createElement("label");
+}
 
-    document.getElementById("please-body").appendChild(newInput);
-    document.getElementById("please-body").appendChild(newLabel);
+async function updateFeel() {
+    let arrayPlease = [
+        "Terrible.",
+        "Guilty.",
+        "Stupid.",
+        "Sorry.",
+        "Blameless.",
+        "Vindicated.",
+        "Better now.",
+    ];
 
-    newLabel.id= "please1";
+    for (let i = 0; i < arrayPlease.length; i++) {
+        const labelStr = arrayPlease[i];
 
-    let str="Forgive Me."
-    newLabel.innerText= str;
+        let newInput = document.createElement("INPUT");
+        newInput.setAttribute("type", "checkbox");
+        let newLabel = document.createElement("label");
+        let breakLine = document.createElement("br");
+
+        document.getElementById("feel-body").appendChild(newInput);
+        document.getElementById("feel-body").appendChild(newLabel);
+        document.getElementById("feel-body").appendChild(breakLine);
+
+        newLabel.id = "feel" + i;
+        newLabel.innerText = labelStr;
+    }
+}
+
+async function updatePlease() {
+    let arrayPlease = [
+        "Forgive Me.",
+        "Be patient with me.",
+        "Give me another chance.",
+        "Forget it ever happened.",
+        "Do not try to get even.",
+        "Do not call the police.",
+        "Do not tell my parents.",
+    ];
+
+    for (let i = 0; i < arrayPlease.length; i++) {
+        const labelStr = arrayPlease[i];
+
+        let newInput = document.createElement("INPUT");
+        newInput.setAttribute("type", "checkbox");
+        let newLabel = document.createElement("label");
+        let breakLine = document.createElement("br");
+
+        document.getElementById("please-body").appendChild(newInput);
+        document.getElementById("please-body").appendChild(newLabel);
+        document.getElementById("please-body").appendChild(breakLine);
+
+        newLabel.id = "please" + i;
+        newLabel.innerText = labelStr;
+    }
 }
